305d059724296e101d7957124c429e42a4633dc0
Spencer Thomas <srt4@uw.edu>
2012-04-14 17:37:14 -0700

    Adding backend

---
+++ backend/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WineCheese::Application.config.secret_token = '422e42a368081743fce90c9248c1e76d446b990b875b442d704bb9641dcb8f081f9c0bf1c586b4bc91b8ce1ba3d9f470d7267acb4c9e968a25c17fbadad35e3c'
--- src/com/winecheesebeer/WineCheeseBeerActivity.java
+++ src/com/winecheesebeer/WineCheeseBeerActivity.java
@@ -1,11 +1,23 @@
 package com.winecheesebeer;
 
+import java.io.IOException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -32,9 +44,25 @@ public class WineCheeseBeerActivity extends Activity {
     	  if (scanResult != null) {
     	    // handle scan result
     		TextView t = (TextView) findViewById(R.id.barCode);
-    		t.setText(scanResult.getContents());
+    		try {
+				t.setText((String)getBarcode(scanResult.getContents()).getString("name"));
+			} catch (Exception e) {
+				 e.printStackTrace();
+			}
     	  }
     	  // else continue with any other code you need in the method
     	  // ...
+    }
+    
+    private JSONObject getBarcode(String barcode) throws ClientProtocolException, IOException, JSONException {
+    	HttpClient hc = new DefaultHttpClient();
+    	
+    	HttpGet g = new HttpGet("http://192.168.1.2:3000/items/" + barcode + ".json");
+    	Log.v("URL", "http://192.168.1.2:3000/items/" + barcode + ".json");
+    	HttpResponse hr = hc.execute(g);
+    	
+    	String response = EntityUtils.toString(hr.getEntity());
+    	Log.v("Response", (String) new JSONObject(response).get("name"));
+    	return new JSONObject(response);
     }
 }
