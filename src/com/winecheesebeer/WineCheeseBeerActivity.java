16f827ac98c36c5e889108c37c480fa4d9bd1222
Spencer Thomas <srt4@uw.edu>
2012-04-15 12:11:13 -0700

    Adding main activity again

--- src/com/winecheesebeer/WineCheeseBeerActivity.java
+++ src/com/winecheesebeer/WineCheeseBeerActivity.java
@@ -2,13 +2,16 @@ package com.winecheesebeer;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.ParseException;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -28,13 +31,22 @@ import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+import com.winecheesebeer.models.Ingredient;
+import com.winecheesebeer.models.Item;
+import com.winecheesebeer.models.ItemCollection;
 
 public class WineCheeseBeerActivity extends Activity {
+	
+	private ItemCollection<String, Item> ic;
+	 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        ic = new ItemCollection<String, Item>();
+        fillIC();
         Button b = (Button) findViewById(R.id.scan);
         b.setOnClickListener(new OnClickListener() {
 			@Override
@@ -45,33 +57,58 @@ public class WineCheeseBeerActivity extends Activity {
 			
         });
         
-        ListView lv = (ListView) findViewById(R.id.list);
-        String[] ingredients = new String[] {"water", "poop", "carrots"};
-        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ingredients));
+
         
-        lv.setOnItemClickListener(new OnItemClickListener() {
+        /*lv.setOnItemClickListener(new OnItemClickListener() {
         public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
               // When clicked, show a toast with the TextView text
               Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
                   Toast.LENGTH_SHORT).show();
             }
           });
-   
+   	   */
     }
     
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
     	  IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+    	  
     	  if (scanResult != null) {
     	    // handle scan result
     		TextView t = (TextView) findViewById(R.id.barCode);
-    		try {
-				t.setText((String)getBarcode(scanResult.getContents()).getString("name"));
-			} catch (Exception e) {
-				 e.printStackTrace();
-			}
+    		Item item = (Item) ic.get(scanResult.getContents());
+    		t.setText(item.name + " - " + item.barcode);
+    		ArrayList<Ingredient> ings = item.ingredients;
+    		ArrayList<String> ingredients = new ArrayList<String>(); 
+    		for (Ingredient singleIng : ings) {
+    			ingredients.add(singleIng.toString());
+    		}
+    		ListView lv = (ListView) findViewById(R.id.list);
+    		lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ingredients));
+    		
+    		//ItemCollection ic = new ItemCollection();
+    		if (false) {
+	    		try {
+	    			
+	    			
+	    			JSONObject j = getBarcode(scanResult.getContents());
+	
+	    			Log.v("TIMING", "Received http data");
+	    			
+	    			JSONArray jarray = j.getJSONArray("ingredients");
+	    			ArrayList<String> ingreds = new ArrayList<String>();
+	    			for (int i = 0; i < jarray.length(); i++) {
+	    				ingreds.add(((JSONObject)jarray.get(i)).getString("name"));
+	    			}
+	    			
+	    	      //  ListView lv = (ListView) findViewById(R.id.list);
+	    	      //  lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ingreds));
+					
+				} catch (Exception e) {
+					 e.printStackTrace();
+				}
     	  }
-    	  // else continue with any other code you need in the method
-    	  // ...
+    	Log.v("TIMING", "Done dislpaying JSON");
+       }
     }
     
     private JSONObject getBarcode(String barcode) throws ClientProtocolException, IOException, JSONException {
@@ -84,5 +121,49 @@ public class WineCheeseBeerActivity extends Activity {
     	String response = EntityUtils.toString(hr.getEntity());
     	Log.v("Response", (String) new JSONObject(response).get("name"));
     	return new JSONObject(response);
+    }
+    
+    private void fillIC() {
+    	HttpClient hc = new DefaultHttpClient();
+    	HttpGet g = new HttpGet("http://spencerrthomas.com:8080/items.json");
+    	HttpResponse hr;
+    	String response; 
+    	JSONArray o; 
+    	
+		try {
+			hr = hc.execute(g);
+			response = EntityUtils.toString(hr.getEntity());
+			o = new JSONArray(response);
+			ArrayList<String> ingreds = new ArrayList<String>();
+			for (int i = 0; i < o.length(); i++) {
+				JSONObject j = o.getJSONObject(i);
+				JSONArray jarray;
+				// if exists ingredients
+    			//ArrayList<String> ingreds = new ArrayList<String>();
+
+				try {
+					jarray = j.getJSONArray("ingredients");
+					Item i1 = new Item(j.getString("name"), j.getString("barcode"));
+					
+	    			for (int k = 0; k < jarray.length(); k++) {
+	    				ingreds.add(((JSONObject)jarray.get(k)).getString("name"));
+	    				i1.addIngredient(new Ingredient(((JSONObject)jarray.get(k)).getString("name")));
+	    			}
+	    			ic.put(j.getString("barcode"), i1);
+				} catch (Exception e) {
+					
+				}
+    			
+    	        ListView lv = (ListView) findViewById(R.id.list);
+    	        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, ingreds));
+				((TextView)findViewById(R.id.barCode)).setText(j.getString("name"));
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return;
+		
     }
 }
